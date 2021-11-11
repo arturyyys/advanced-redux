c659be984f94316134dc2a94f2c9bce4599d6ff9
Arturas Jurevicius <arturyyys@gmail.com>
2021-11-11 21:27:59 +0200

    v261. Finalizing the Fetching Logic

--- store/cart-actions.js
+++ store/cart-actions.js
@@ -7,9 +7,11 @@ export const fetchCartData = () => {
       const response = await fetch(
         "https://react-http-94568-default-rtdb.firebaseio.com/cart.json"
       );
+
       if (!response.ok) {
         throw new Error("Could not fetch cart data!");
       }
+
       const data = await response.json();
 
       return data;
@@ -17,7 +19,12 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -45,7 +52,10 @@ export const sendCartData = (cart) => {
         "https://react-http-94568-default-rtdb.firebaseio.com/cart.json",
         {
           method: "PUT",
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       );
 
@@ -53,8 +63,10 @@ export const sendCartData = (cart) => {
         throw new Error("Sending cart data failed.");
       }
     };
+
     try {
       await sendRequest();
+
       dispatch(
         uiActions.showNotification({
           status: "success",
